b51332f2d6d18ce494211bf1907e5ce5e43926c1
Servontius Turner <servontius.turner@me.com>
2023-12-30 22:59:39 -0600

    Completed exercises
    Adding completed code for Sum of Sequence and Sum of Sequence - the sequel

--- TMCProjects/mooc-java-programming-i/part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ TMCProjects/mooc-java-programming-i/part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -1,4 +1,7 @@
-
+/*
+Implement a program which calculates the sum of a closed interval, and prints it.
+Expect the user to write the smaller number first and then the larger number.
+*/
 import java.util.Scanner;
 
 public class SumOfASequenceTheSequel {
@@ -6,5 +9,18 @@ public class SumOfASequenceTheSequel {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("First number? ");
+        int startingNumber = Integer.valueOf(scanner.nextLine());
+
+        System.out.print("Last number? ");
+        int endingNumber = Integer.valueOf(scanner.nextLine());
+
+        int sumOfNumbers = 0;
+
+        for (int i = startingNumber; i <= endingNumber; i++) {
+            sumOfNumbers = sumOfNumbers + i;
+        }
+
+        System.out.print("The sum is: " + sumOfNumbers);
     }
 }
