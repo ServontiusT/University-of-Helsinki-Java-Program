f3d649503b583538405b2903f634bb012410dbd2
Servontius Turner <servontius.turner@me.com>
2024-01-05 22:32:55 -0600

    Completed From on to Parameter

--- TMCProjects/mooc-java-programming-i/part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
+++ TMCProjects/mooc-java-programming-i/part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -1,9 +1,17 @@
-
+/*
+Create the following method in the exercise template: public static void printUntilNumber(int number).
+It should print the numbers from one to the number passed as a parameter.
+ */
 
 public class FromOneToParameter {
 
     public static void main(String[] args) {
-
+        printUntilNumber(5);
     }
 
+    public static void printUntilNumber(int number) {
+        for(int i = 1; i <= number; i++) {
+            System.out.println(i);
+        }
+    }
 }
