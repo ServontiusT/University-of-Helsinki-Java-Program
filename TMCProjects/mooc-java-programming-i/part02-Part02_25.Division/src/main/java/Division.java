69f6997554e76373edb4fc492c6a94f9c13e6ec2
Servontius Turner <servontius.turner@me.com>
2024-01-05 22:32:17 -0600

    Completed Division

--- TMCProjects/mooc-java-programming-i/part02-Part02_25.Division/src/main/java/Division.java
+++ TMCProjects/mooc-java-programming-i/part02-Part02_25.Division/src/main/java/Division.java
@@ -1,4 +1,9 @@
-
+/*
+Write a method public static void division(int numerator, int denominator) that prints the result of the division of
+the numerator by the denominator.
+Keep in mind that the result of the division of the integers is an integer
+in this case we want the result to be a floating point number.
+ */
 
 public class Division {
 
@@ -8,8 +13,11 @@ public class Division {
         // try it out here. Fopr example division(3,5);
         // should print "0.6"
 
-        // division(3, 5);
+        division(3, 5);
     }
 
     // implement the method here
+    public static void division(int numerator, int denominator) {
+        System.out.println((double) numerator / denominator);
+    }
 }
