1de19f9cdb06cd3150b4659f637314ada5ce8a4b
Servontius Turner <servontius.turner@me.com>
2024-01-06 23:00:51 -0600

    Completed Word

--- TMCProjects/mooc-java-programming-i/part02-Part02_28.Word/src/main/java/Word.java
+++ TMCProjects/mooc-java-programming-i/part02-Part02_28.Word/src/main/java/Word.java
@@ -1,4 +1,6 @@
-
+/*
+Write a method public static String word(). The method must return a string of your choice.
+ */
 import java.util.Scanner;
 
 public class Word {
@@ -6,6 +8,12 @@ public class Word {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        word();
+
+    }
+
+    public static String word() {
+        return "Some String";
     }
 
 }
