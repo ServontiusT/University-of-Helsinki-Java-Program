e349b81bf67e47c6993407ecc2c5918e7aaf3104
Servontius Turner <servontius.turner@me.com>
2024-01-06 22:56:04 -0600

    Completed Number Uno

--- TMCProjects/mooc-java-programming-i/part02-Part02_27.NumberUno/src/main/java/NumberUno.java
+++ TMCProjects/mooc-java-programming-i/part02-Part02_27.NumberUno/src/main/java/NumberUno.java
@@ -1,4 +1,6 @@
-
+/*
+Write a method public static int numberUno() that returns the value 1.
+ */
 import java.util.Scanner;
 
 public class NumberUno {
@@ -6,6 +8,10 @@ public class NumberUno {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        numberUno();
     }
 
+    public static int numberUno() {
+        return 1;
+    }
 }
