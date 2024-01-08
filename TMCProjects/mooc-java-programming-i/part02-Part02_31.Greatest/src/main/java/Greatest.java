bbb7262447a4721d5e19cf743b0e888bc38d6627
Servontius Turner <servontius.turner@me.com>
2024-01-07 23:48:56 -0600

    Completed Greatest

--- TMCProjects/mooc-java-programming-i/part02-Part02_31.Greatest/src/main/java/Greatest.java
+++ TMCProjects/mooc-java-programming-i/part02-Part02_31.Greatest/src/main/java/Greatest.java
@@ -1,10 +1,19 @@
-
+/*
+* Define a method called greatest that takes three numbers and returns the greatest of them.
+* If there are multiple greatest values, returning one of them is enough.
+* Printing will take place in the main program.
+*/
 public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
         //write some code here
-
-        return -1;
+        if (number1 > number2 && number1 > number3) {
+            return number1;
+        } else if (number2 > number3) {
+            return number2;
+        } else {
+            return number3;
+        }
     }
 
     public static void main(String[] args) {
