5690b0f024f3eb384e5ec0476ef4e0d7ea5d457a
Servontius Turner <servontius.turner@me.com>
2024-01-11 23:11:43 -0600

    Still working on christmasTree method. Currently, prints tree upside down.

--- TMCProjects/mooc-java-programming-i/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ TMCProjects/mooc-java-programming-i/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -1,4 +1,9 @@
-
+/*
+* Define a method called christmasTree(int height) that prints the correct Christmas tree.
+* The Christmas tree consists of a triangle with the specified height as well as the base.
+* The base is two stars high and three stars wide, and is placed at the center of the triangle's bottom.
+* The tree is to be constructed by using the methods printSpaces and printStars.
+*/
 public class AdvancedAstrology {
 
     public static void printStars(int number) {
@@ -11,7 +16,7 @@ public class AdvancedAstrology {
 
     public static void printSpaces(int number) {
         // part 1 of the exercise
-        for (int spaces = 1; spaces <= number - 1; spaces++) {
+        for (int spaces = number - 1; spaces >= 0; spaces--) {
             System.out.print(" ");
         }
     }
@@ -19,14 +24,24 @@ public class AdvancedAstrology {
     public static void printTriangle(int size) {
         // part 2 of the exercise
         for (int row = 1; row <= size; row++) {
-            printSpaces(size - row + 1);
+            printSpaces(size - row);
             printStars(row);
         }
     }
 
     public static void christmasTree(int height) {
         // part 3 of the exercise
-        // Print the top part of the tree
+
+
+        for (int i = height - 1; i >= 1; i--) {
+            printSpaces(height - i);
+            printStars(2 * i - 1);
+        }
+        // Print the base of the tree
+        for (int i = 1; i <= 2; i++) {
+            printSpaces(height - 2);
+            printStars(3);
+        }
     }
 
     public static void main(String[] args) {
