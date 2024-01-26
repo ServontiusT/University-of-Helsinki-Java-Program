bc70170cd1207a2b500db350c7c6eb5ee51412b4
Servontius Turner <servontius.turner@me.com>
2024-01-25 20:41:34 -0600

    Completed Sum with provided data from MOOC

--- TMCProjects/mooc-java-programming-i/part03-Part03_16.Sum/src/main/java/Sum.java
+++ TMCProjects/mooc-java-programming-i/part03-Part03_16.Sum/src/main/java/Sum.java
@@ -1,3 +1,7 @@
+/*
+* Create the method public static int sum(ArrayList<Integer> numbers) in the exercise template.
+* The method is to return the sum of the numbers in the parameter list.
+*/
 
 import java.util.ArrayList;
 
@@ -5,6 +9,23 @@ public class Sum {
 
     public static void main(String[] args) {
         // Try your method here
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+
+        System.out.println(sum(numbers));
+    }
+
+    public static int sum(ArrayList<Integer> numbers){
+        int sumOfNumbers = 0;
+
+        for (int number:numbers) {
+            sumOfNumbers += number;
+        }
+
+        return sumOfNumbers;
     }
 
 }
