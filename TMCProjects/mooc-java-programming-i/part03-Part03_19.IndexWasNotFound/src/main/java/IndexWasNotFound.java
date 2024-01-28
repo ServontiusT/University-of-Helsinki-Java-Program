7f1db0f5c05ca11c823a49f423ce00a29ced8157
Servontius Turner <servontius.turner@me.com>
2024-01-28 17:16:25 -0600

    Completed IndexWasNotFound

--- TMCProjects/mooc-java-programming-i/part03-Part03_19.IndexWasNotFound/src/main/java/IndexWasNotFound.java
+++ TMCProjects/mooc-java-programming-i/part03-Part03_19.IndexWasNotFound/src/main/java/IndexWasNotFound.java
@@ -1,4 +1,8 @@
-
+/*
+* Complete the program so that it asks the user for a number to search in the array.
+* If the array contains the given number, the program tells the index containing the number.
+* If the array doesn't contain the given number, the program will advise that the number wasn't found.
+*/
 
 import java.util.Scanner;
 
@@ -17,9 +21,16 @@ public class IndexWasNotFound {
         array[7] = 7;
 
         System.out.print("Search for? ");
-        int searching = Integer.valueOf(scanner.nextLine());
+        int searching = Integer.parseInt(scanner.nextLine());
 
         // Implement the search functionality here
-    }
+        for (int index = 0; index <= array.length - 1; index ++) {
+            if (array[index] == searching) {
+                System.out.println(searching + " is at index " + index + ".");
+                return;
+            }
+        }
 
+        System.out.println(searching + " was not found.");
+    }
 }
