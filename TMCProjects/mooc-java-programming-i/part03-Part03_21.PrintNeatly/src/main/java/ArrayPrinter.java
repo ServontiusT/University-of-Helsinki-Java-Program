81f190f4e8cb4b31808aaaf78632ffa280474c66
Servontius Turner <servontius.turner@me.com>
2024-01-28 21:21:06 -0600

    Completed PrintNeatly

--- TMCProjects/mooc-java-programming-i/part03-Part03_21.PrintNeatly/src/main/java/ArrayPrinter.java
+++ TMCProjects/mooc-java-programming-i/part03-Part03_21.PrintNeatly/src/main/java/ArrayPrinter.java
@@ -1,4 +1,10 @@
+/*
+* Complete the method public static void printNeatly(int[] array) in the class named 'ArrayPrinter'
+* to make it print the numbers of the array it receives more neatly.
+* There should be a whitespace and a comma between each number. don't put a comma after the last number.
 
+* Print the numbers on one line using System.out.print.
+*/
 public class ArrayPrinter {
 
     public static void main(String[] args) {
@@ -9,5 +15,14 @@ public class ArrayPrinter {
 
     public static void printNeatly(int[] array) {
         // Write some code in here
+        int endIndex = array.length - 1;
+
+        for (int number = 0; number <= array.length - 1; number++) {
+            if (number < endIndex) {
+                System.out.print(array[number] + ", ");
+            } else {
+                System.out.print(array[number]);
+            }
+        }
     }
 }
