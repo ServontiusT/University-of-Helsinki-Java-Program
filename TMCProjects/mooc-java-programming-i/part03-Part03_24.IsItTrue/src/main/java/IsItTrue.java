32b9ff04c7092c4875651659c3a8cb2e93c2a8c9
Servontius Turner <servontius.turner@me.com>
2024-01-28 22:13:23 -0600

    Completed IsItTrue

--- TMCProjects/mooc-java-programming-i/part03-Part03_24.IsItTrue/src/main/java/IsItTrue.java
+++ TMCProjects/mooc-java-programming-i/part03-Part03_24.IsItTrue/src/main/java/IsItTrue.java
@@ -1,4 +1,7 @@
-
+/*
+* Write a program that asks the user for a string.
+* If the user writes the string "true", the program prints "You got it right!", otherwise it prints "Try again!".
+*/
 import java.util.Scanner;
 
 public class IsItTrue {
@@ -6,5 +9,12 @@ public class IsItTrue {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String userString = scanner.nextLine();
+
+        if (userString.equals("true") || userString.equals("True")) {
+            System.out.println("You got it right!");
+        } else {
+            System.out.println("Try again!");
+        }
     }
 }
