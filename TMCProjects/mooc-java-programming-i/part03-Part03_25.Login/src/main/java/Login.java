f8e132a24fef322d21bec1e5541a49df5782ffc4
Servontius Turner <servontius.turner@me.com>
2024-01-29 23:31:30 -0600

    Completed Login

--- TMCProjects/mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
+++ TMCProjects/mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
@@ -1,4 +1,11 @@
+/*
+Write a program that recognizes the following users:
 
+username	password
+alex	sunshine
+emma	haskell
+The program either shows a personal message or informs of an incorrect username or password.
+*/
 import java.util.Scanner;
 
 public class Login {
@@ -6,5 +13,32 @@ public class Login {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter username: ");
+        String authenticatingUser = scanner.nextLine();
+
+        System.out.print("Enter password: ");
+        String authenticatingPasswd = scanner.nextLine();
+
+
+        String[] usernames = {"alex", "emma"};
+        String[] passwords = {"sunshine", "haskell"};
+
+        boolean usernameValidated = false;
+        boolean passwdValidated = false;
+
+        for (int i = 0; i <= usernames.length - 1; i++) {
+            if (usernames[i].equals(authenticatingUser)) {
+                usernameValidated = true;
+            }
+            if (passwords[i].equals(authenticatingPasswd)) {
+                passwdValidated = true;
+            }
+        }
+
+        if (usernameValidated && passwdValidated) {
+            System.out.println("You have successfully logged in");
+        } else {
+            System.out.println("Incorrect username or password");
+        }
     }
 }
