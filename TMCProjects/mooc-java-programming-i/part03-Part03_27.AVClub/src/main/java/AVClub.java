ad61316e69c65f4d742af0dfb4cc4ca80604dc18
Servontius Turner <servontius.turner@me.com>
2024-01-30 09:33:55 -0600

    Completed AVClub

--- TMCProjects/mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ TMCProjects/mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -1,4 +1,8 @@
-
+/*
+Write a program that reads user input until an empty line.
+For each non-empty string, the program splits the string by spaces   and then prints the pieces that contain av,
+each on a new line.
+*/
 import java.util.Scanner;
 
 public class AVClub {
@@ -6,6 +10,25 @@ public class AVClub {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String userSentence = scanner.nextLine();
 
+            if (userSentence.isEmpty()) {
+                break;
+            } else {
+                splitString(userSentence);
+            }
+        }
+
+    }
+
+    public static void splitString(String sentence) {
+        String[] sentenceArray = sentence.split(" ");
+
+        for (String word: sentenceArray) {
+            if (word.contains("av")) {
+                System.out.println(word);
+            }
+        }
     }
 }
