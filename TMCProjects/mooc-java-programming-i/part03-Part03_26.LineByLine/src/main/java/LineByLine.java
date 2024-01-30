a06887e1b2dd81bfe8f94c8c516b2e7e7e5c46b2
Servontius Turner <servontius.turner@me.com>
2024-01-30 09:25:19 -0600

    Completed LineByLine

--- TMCProjects/mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ TMCProjects/mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -1,4 +1,8 @@
-
+/*
+* Write a program that reads strings from the user.
+* If the input is empty, the program stops reading input and halts.
+* For each non-empty input it splits the string input by whitespaces   and prints each part of the string on a new line.
+*/
 import java.util.Scanner;
 
 public class LineByLine {
@@ -6,6 +10,22 @@ public class LineByLine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while(true){
+            String userString = scanner.nextLine();
+            if (userString.isEmpty()) {
+                break;
+            } else {
+                splitString(userString);
+                continue;
+            }
+        }
+    }
 
+    public static void splitString (String sentence) {
+        String[] sentenceArray = sentence.split(" ");
+
+        for (String word: sentenceArray) {
+            System.out.println(word);
+        }
     }
 }
