c59dec5f23c4362389156063025e5f1304c2c1ba
Servontius Turner <servontius.turner@me.com>
2024-01-30 21:19:51 -0600

    Completed First Words

--- TMCProjects/mooc-java-programming-i/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ TMCProjects/mooc-java-programming-i/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -1,4 +1,7 @@
-
+/*
+Write a program that reads user input until an empty line is entered.
+For each non-empty line the program splits the string by spaces   and prints the first part of the string.
+*/
 import java.util.Scanner;
 
 public class FirstWords {
@@ -6,6 +9,19 @@ public class FirstWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String userSentence = scanner.nextLine();
+            if (userSentence.isEmpty()) {
+                return;
+            } else {
+                selectFirstInString(userSentence);
+            }
+        }
+    }
 
+    public static void selectFirstInString(String sentence) {
+        String[] seperatedSentence = sentence.split(" ");
+
+        System.out.println(seperatedSentence[0]);
     }
 }
