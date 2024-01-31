1eece5763c050fc82c9b512a48b7a809e17539fa
Servontius Turner <servontius.turner@me.com>
2024-01-31 09:00:38 -0600

    Completed LastWords

--- TMCProjects/mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ TMCProjects/mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -1,4 +1,7 @@
-
+/*
+* Write a program that reads user input until an empty line is entered.
+* For each non-empty line the program splits the string by spaces   and prints the last part of the string.
+*/
 import java.util.Scanner;
 
 public class LastWords {
@@ -6,6 +9,22 @@ public class LastWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.isEmpty()) {
+                return;
+            } else {
+                printLastWord(userInput);
+            }
+        }
+    }
 
+    public static void printLastWord(String sentence) {
+        String[] splitSentenceArray = sentence.split(" ");
+
+        int arraySize = splitSentenceArray.length;
+        String lastWord = splitSentenceArray[arraySize - 1];
+
+        System.out.println(lastWord);
     }
 }
