efb4f4ee27d1e21fc235554ed9eac73cbabd2161
Servontius Turner <servontius.turner@me.com>
2024-02-04 13:53:26 -0600

    Completed YourFirstBankTransfer

--- TMCProjects/mooc-java-programming-i/part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -1,8 +1,24 @@
+/*
+Write a program that:
 
+Creates an account named "Matthews account" with the balance 1000
+Creates an account named "My account" with the balance 0
+Withdraws 100.0 from Matthew's account
+Deposits 100.0 to "my account"
+Prints both the accounts
+*/
 public class YourFirstBankTransfer {
 
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // write your program here
+        Account mattsAccount = new Account("Matthews account", 1000);
+        Account myAccount = new Account("My account", 0);
+
+        mattsAccount.withdrawal(100);
+        myAccount.deposit(100);
+
+        System.out.println(mattsAccount);
+        System.out.println(myAccount);
     }
 }
