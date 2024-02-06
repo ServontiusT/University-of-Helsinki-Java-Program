425b3d46676bcf3f68edb41cca328b440789b8d9
Servontius Turner <servontius.turner@me.com>
2024-02-05 22:15:04 -0600

    Updated DecreasingCounter to include the three parts for completion

--- TMCProjects/mooc-java-programming-i/part04-Part04_08.DecreasingCounter/src/main/java/DecreasingCounter.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_08.DecreasingCounter/src/main/java/DecreasingCounter.java
@@ -1,7 +1,14 @@
 /*
+Part 1
 Implement the decrement() method in the class body in such a way that it decrements the value variable of the object it's being called on by one.
 Once you're done with the decrement() method, the main program of the previous example should work to produce the example output.
 
+Part 2
+Implement the decrement() in such a way that the counter's value never becomes negative.
+This means that if the value of the counter is 0, it cannot be decremented. A conditional statement is useful here.
+
+Part 3
+Create the method public void reset() for the counter that resets the value of the counter to 0. For example:
  */
 public class DecreasingCounter {
 
