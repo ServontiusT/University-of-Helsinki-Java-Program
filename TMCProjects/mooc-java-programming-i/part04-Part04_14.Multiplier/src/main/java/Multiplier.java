5d9224f8d7702df61b9a0f8c061978f9cf14cf5d
Servontius Turner <servontius.turner@me.com>
2024-02-11 16:07:49 -0600

    Completed Multiplier

---
+++ TMCProjects/mooc-java-programming-i/part04-Part04_14.Multiplier/src/main/java/Multiplier.java
@@ -0,0 +1,12 @@
+public class Multiplier {
+    private int multiplier; //The number to be multiplied
+
+    public Multiplier(int number) {
+        this.multiplier = number;
+    }
+
+    public int multiply(int number) {
+        return this.multiplier * number;
+    }
+}
+
