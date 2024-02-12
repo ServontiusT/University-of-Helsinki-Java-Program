91e0a1fd8f512fbfb4cec1b3131817955e7c58ae
Servontius Turner <servontius.turner@me.com>
2024-02-12 12:44:09 -0600

    Completed Statistics

--- TMCProjects/mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -12,5 +12,30 @@ public class MainProgram {
         // correct order in the main program. First the object that tracks the total
         // sum, secondly the object that tracks the sum of even numbers, 
         // and lastly the one that tracks the sum of odd numbers!
+
+        Statistics statistics = new Statistics();
+        Statistics evenStatistics = new Statistics();
+        Statistics oddStatistics = new Statistics();
+
+        int userNumber = 0;
+        System.out.println("Enter numbers:");
+
+        while(true) {
+            userNumber = Integer.parseInt(scanner.nextLine());
+            if(userNumber == -1) {
+                break;
+            } else if(userNumber % 2 == 0){
+              statistics.addNumber(userNumber);
+              evenStatistics.addNumber(userNumber);
+            } else {
+                statistics.addNumber(userNumber);
+                oddStatistics.addNumber(userNumber);
+            }
+        }
+
+        System.out.println("Count: " + statistics.getCount());
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Sum of even numbers: " + evenStatistics.sum());
+        System.out.println("Sum of odd numbers: " + oddStatistics.sum());
     }
 }
--- TMCProjects/mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,58 @@
+/*
+Part 1:
+Create a class Statistics that has the following functionality (the file for the class is provided in the exercise template):
+
+a method addNumber adds a new number to the statistics
+a method getCount tells the number of added numbers
+The class does not need to store the added numbers anywhere, it is enough for it to remember their count.
+At this stage, the addNumber method can even neglect the numbers being added to the statistics, since the only thing being stored is the count of numbers added.
+
+Part 2:
+Expand the class with the following functionality:
+
+the sum method tells the sum of the numbers added (the sum of an empty number statistics object is 0)
+the average method tells the average of the numbers added (the average of an empty number statistics object is 0
+
+Part 3:
+Write a program that asks the user for numbers until the user enters -1. The program will then provide the sum of the numbers.
+
+The program should use a Statistics object to calculate the sum.
+
+NOTE: Do not modify the Statistics class in this part. Instead, implement the program for calculating the sum by making use of it.
+
+Part 4:
+Change the previous program so that it also calculates the sum of even and odd numbers.
+
+NOTE: Define three Statistics objects in the program. Use the first to calculate the sum of all numbers, the second to calculate the sum of even numbers, and the third to calculate the sum of odd numbers.
+
+For the test to work, the objects must be created in the main program in the order they were mentioned above (i.e., first the object that sums all the numbers, then the one that sums the even ones, and then finally the one that sums the odd numbers)!
+
+NOTE: Do not change the Statistics class in any way!
+*/
 
 public class Statistics {
+    private int number;
+    private int counter;
+
+    public Statistics() {}
+
+    public void addNumber(int number) {
+        this.counter++;
+        this.number+= number;
+    }
+
+    public int getCount() {
+        return this.counter;
+    }
+
+    public int sum() {
+        return this.number;
+    }
+    public double average() {
+        if (this.number > 0) {
+            return (double) this.number / (double) this.counter;
+        } else {
+            return 0.0;
+        }
+    }
 }
