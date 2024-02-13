b26b9f170867118c732197700c7cce62048de60b
Servontius Turner <servontius.turner@me.com>
2024-02-13 09:21:22 -0600

    Completed PersonalInformationCollection

--- TMCProjects/mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -1,4 +1,12 @@
+/*
+The program described here should be implemented in the class PersonalInformationCollection.
+NB! Do not modify the class PersonalInformation.
 
+After the user has entered the last set of details (they enter an empty first name), exit the repeat statement.
+
+Then print the collected personal information so that each entered object is printed in the following format:
+first and last names separated by a space (you don't print the identification number). 
+*/
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,5 +18,30 @@ public class PersonalInformationCollection {
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        String firstName;
+        String lastName;
+        String userID;
+
+        while(true) {
+            System.out.print("");
+            firstName = scanner.nextLine();
+
+            if (firstName.isEmpty()) {
+                break;
+            } else {
+                System.out.print("");
+                lastName = scanner.nextLine();
+
+                System.out.print("");
+                userID = scanner.nextLine();
+
+                PersonalInformation newUser = new PersonalInformation(firstName, lastName, userID);
+                infoCollection.add(newUser);
+            }
+        }
+
+        for(PersonalInformation person: infoCollection) {
+            System.out.println(person.getFirstName() + " " + person.getLastName());
+        }
     }
 }
