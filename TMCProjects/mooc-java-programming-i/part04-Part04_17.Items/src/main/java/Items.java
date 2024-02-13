2f32c3aa5674c7cc44cfb9152ad5952eb9fe2148
Servontius Turner <servontius.turner@me.com>
2024-02-12 22:41:45 -0600

    Completed Items

--- TMCProjects/mooc-java-programming-i/part04-Part04_17.Items/src/main/java/Items.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_17.Items/src/main/java/Items.java
@@ -1,4 +1,13 @@
+/*
+Implement the class Items described here. NB! Don't modify the class Item.
 
+Write a program that reads names of items from the user.
+If the name is empty, the program stops reading.
+Otherwise, the given name is used to create a new item, which you will then add to the items list.
+
+Having read all the names, print all the items by using the toString method of the Item class.
+The implementation of the Item class keeps track of the time of creation, in addition to the name of the item.
+*/
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,5 +19,20 @@ public class Items {
         ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        String userNames;
+        while(true) {
+            System.out.print("Name: ");
+            userNames = scanner.nextLine();
+
+            if (userNames.isEmpty()) {
+                break;
+            } else {
+                items.add(new Item(userNames));
+            }
+        }
+
+        for(Item name: items) {
+            System.out.println(name);
+        }
     }
 }
