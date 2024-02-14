763b8858c905ef19d5fdeaffc3f7d285d3fdf8b5
Servontius Turner <servontius.turner@me.com>
2024-02-13 22:36:54 -0600

    Completed TelevisionProgram

--- TMCProjects/mooc-java-programming-i/part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -1,3 +1,14 @@
+/*
+In the exercise template there is a ready-made class TelevisionProgram, representing a television program.
+The class has object variables name and duration, a constructor, and a few methods.
+
+Implement a program that begins by reading television programs from the user.
+When the user inputs an empty string as the name of the program, the program stops reading programs.
+
+After this the user is queried for a maximum duration.
+Once the maximum is given,
+the program proceeds to list all the programs whose duration is smaller or equal to the specified maximum duration.
+*/
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -9,5 +20,28 @@ public class Main {
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while(true) {
+            System.out.print("");
+            String programName = scanner.nextLine();
+
+            if(programName.isEmpty()) {
+                break;
+            } else {
+                System.out.print("");
+                int programDuration = Integer.parseInt(scanner.nextLine());
+
+                TelevisionProgram currentLineup = new TelevisionProgram(programName, programDuration);
+                programs.add(currentLineup);
+            }
+        }
+
+        System.out.println("Program's maximum duration?");
+        int maxDuration = Integer.parseInt(scanner.nextLine());
+
+        for(TelevisionProgram program: programs) {
+            if(program.getDuration() <= maxDuration) {
+                System.out.println(program.getName());
+            }
+        }
     }
 }
