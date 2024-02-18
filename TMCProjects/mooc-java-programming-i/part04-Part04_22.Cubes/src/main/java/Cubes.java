6e93829116c87d51d21ee0bc60edc6711fb42227
Servontius Turner <servontius.turner@me.com>
2024-02-18 10:44:41 -0600

    completed Cubes

--- TMCProjects/mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -6,5 +6,19 @@ public class Cubes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String userNumber;
+        int numberAsInt;
+
+        while(true) {
+            System.out.print("");
+            userNumber = scanner.nextLine();
+
+            if(userNumber.equals("end")) {
+                break;
+            } else {
+                numberAsInt = Integer.parseInt(userNumber);
+                System.out.println(numberAsInt * numberAsInt * numberAsInt);
+            }
+        }
     }
 }
