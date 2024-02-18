6db2fae15608608ad8940a041051087afe653af9
Servontius Turner <servontius.turner@me.com>
2024-02-18 10:45:02 -0600

    completed PrintingAFile

--- TMCProjects/mooc-java-programming-i/part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
@@ -1,10 +1,17 @@
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class PrintingAFile {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Scanner fileScanner = new Scanner(Paths.get("data.txt"));
 
+        while(fileScanner.hasNextLine()) {
+            String row = fileScanner.nextLine();
+
+            System.out.println(row);
+        }
     }
 }
