5009cdaaf8864dce2cad2e3ce0a45709a2ca4b3c
Servontius Turner <servontius.turner@me.com>
2024-02-19 12:40:20 -0600

    Completed GuestListFromAFile

--- TMCProjects/mooc-java-programming-i/part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
@@ -1,11 +1,18 @@
+/*
+The exercise template comes ready with functionality for the guest list application.
+It checks whether names entered by the user are on the guest list.
 
+However, the program is missing the functionality needed for reading the guest list.
+Modify the program so that the names on the guest list are read from the file.
+*/
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GuestListFromAFile {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Name of the file:");
@@ -13,9 +20,17 @@ public class GuestListFromAFile {
 
         ArrayList<String> list = new ArrayList<>();
         // implement reading the file here.
+        Scanner fileReader = new Scanner(Paths.get(file));
+
+        for (Scanner name = fileReader; name.hasNext(); ) {
+            String row = name.next();
+            list.add(row);
+        }
+
         System.out.println("");
 
         System.out.println("Enter names, an empty line quits.");
+
         while (true) {
             String name = scanner.nextLine();
             if (name.isEmpty()) {
