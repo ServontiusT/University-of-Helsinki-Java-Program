dff052bc60c976ef7e54c5fdbf0cb75c48aa35c4
Servontius Turner <servontius.turner@me.com>
2024-03-25 10:15:57 -0500

    Completed problem. Reminder, RTFM.

--- TMCProjects/mooc-java-programming-i/part05-Part05_01.OneMinute/src/main/java/Program.java
+++ TMCProjects/mooc-java-programming-i/part05-Part05_01.OneMinute/src/main/java/Program.java
@@ -5,7 +5,15 @@ public class Program {
 
     public static void main(String[] args) {
         // You can test your program here
+        Timer t = new Timer();
+        int i = 0;
 
+        //When the advance method is called 1222 times, the print should be "12:22".
+        while (i < 1222) {
+            t.advance();
+            i++;
+        }
+        System.out.println(t);
 
     }
 }
--- TMCProjects/mooc-java-programming-i/part05-Part05_01.OneMinute/src/main/java/Timer.java
+++ TMCProjects/mooc-java-programming-i/part05-Part05_01.OneMinute/src/main/java/Timer.java
@@ -1,45 +1,21 @@
-public class Timer {
+import java.time.Clock;
 
-    private int hundredthsOfSecondHand;
-    private int secondsHand;
+public class Timer {
+    private ClockHand seconds;
+    private ClockHand hundredths;
 
     public Timer() {
-        this.hundredthsOfSecondHand = 0;
-        this.secondsHand = 0;
+        this.seconds = new ClockHand(60);
+        this.hundredths = new ClockHand(100);
     }
 
     public void advance() {
-        int hundredthsLimit = 100;
-        int secondsLimit = 60;
+        hundredths.advance();
 
-        if (this.hundredthsOfSecondHand <= hundredthsLimit) {
-            this.hundredthsOfSecondHand++;
-        }
-        if (this.hundredthsOfSecondHand > 100){
-            this.secondsHand++;
-            this.hundredthsOfSecondHand = 0;
-        }
-        if (this.secondsHand > secondsLimit) {
-            this.secondsHand = 0;
+        if (hundredths.value() == 0) {
+            seconds.advance();
         }
     }
 
-    public String toString () {
-        String secondsToString = "";
-        String hundredthsToString = "";
-        int timeLimit = 10;
-
-        if (this.hundredthsOfSecondHand < timeLimit) {
-            hundredthsToString = "0" + this.hundredthsOfSecondHand;
-        }
-        if (this.secondsHand < timeLimit) {
-            secondsToString = "0" + this.secondsHand;
-        }
-
-        if (this.secondsHand > timeLimit && this.hundredthsOfSecondHand > timeLimit) {
-            return this.secondsHand + ":" + this.hundredthsOfSecondHand;
-        } else {
-            return secondsToString + ":" + hundredthsToString;
-        }
-    }
+    public String toString() { return seconds + ":" + hundredths;}
 }
