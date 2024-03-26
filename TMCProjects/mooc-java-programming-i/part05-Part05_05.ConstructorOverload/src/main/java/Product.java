3fc510cb8abda310c50e715a3083a1dc647b67b6
Servontius Turner <servontius.turner@me.com>
2024-03-26 12:38:03 -0500

    Completed Product

--- TMCProjects/mooc-java-programming-i/part05-Part05_05.ConstructorOverload/src/main/java/Product.java
+++ TMCProjects/mooc-java-programming-i/part05-Part05_05.ConstructorOverload/src/main/java/Product.java
@@ -1,4 +1,14 @@
+/*
 
+The exercise template has a class Product, which represents a product in a shop. Every product has a name, location and weight.
+
+Add the following three constructors to the Product class:
+
+public Product(String name) creates a product with the given name. Its location is set to "shelf" and its weight is set to 1.
+public Product(String name, String location) creates a product with the given name and the given location. Its weight is set to 1.
+public Product(String name, int weight) creates a product with the given name and the given weight. Its location is set to "shelf".
+
+ */
 public class Product {
 
     private String name;
@@ -11,6 +21,17 @@ public class Product {
         this.weight = weight;
     }
 
+    public Product(String name) {
+        this(name, "shelf", 1);
+    }
+
+    public Product(String name, String location) {
+        this(name, location, 1);
+    }
+
+    public Product(String name, int weight) {
+        this(name, "shelf", weight);
+    }
     
     public String getName() {
         return name;
