4e8df5ed2fea4968be5e901306eacd1fb2433bb0
Servontius Turner <servontius.turner@me.com>
2024-03-28 21:59:48 -0500

    Completed Counter

--- TMCProjects/mooc-java-programming-i/part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
+++ TMCProjects/mooc-java-programming-i/part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
@@ -1,4 +1,18 @@
+/*
+Part 1
+Implement a class called Counter. The class contains a number whose value can be incremented and decremented.
+The class must have the following constructors:
 
+public Counter(int startValue) sets the start value of the counter to startValue.
+
+public Counter() sets the start value of the counter to 0.
+
+And the following methods:
+
+public int value() returns the current value of the counter
+public void increase() increases the value by 1
+public void decrease() decreases the value by 1
+ */
 public class MainProgram {
 
     public static void main(String[] args) {
